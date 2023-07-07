6f3a8061b418ff892dcdb523e211d64bf1829aa6
ozcanpolat <93453642+ozcanpolat@users.noreply.github.com>
2023-07-07 14:12:33 +0300

    Donguler1

---
+++ Donguler1/Donguler1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Donguler1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan başlangıç, bitiş ve artış miktarı alınarak aralıktaki
+            // tüm sayıları ekrana yazdırınız.
+
+            Console.Write("Başlangıç değerini girin: ");
+            int baslangic = int.Parse(Console.ReadLine());
+
+            Console.Write("Bitiş değerini girin: ");
+            int bitis = int.Parse(Console.ReadLine());
+
+            Console.Write("Artış değerini girin: ");
+            int artis = int.Parse(Console.ReadLine());
+
+            for(int i=baslangic; i<=bitis; i=i+artis)
+            {
+                Console.WriteLine(i);
+   
+            }
+        }
+    }
+}
